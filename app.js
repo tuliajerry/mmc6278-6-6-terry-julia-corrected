2e84bdfdf4fe0a4cddb855f2861246f84c9eb1d5
Julia Terry <156032724+tuliajerry@users.noreply.github.com>
2024-10-13 17:10:46 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-const express = require('express')
-const apiRoutes = require('./routes/api-routes')
-const htmlRoutes = require('./routes/html-routes')
-const app = express()
-// TODO: Require Exphbs
+const express = require('express');
+const apiRoutes = require('./routes/api-routes');
+const htmlRoutes = require('./routes/html-routes');
+const exphbs = require('express-handlebars'); 
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
 
-// TODO: Add the handlebars setup code
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 
-app.use(express.static('public'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-app.use('/', htmlRoutes)
-app.use('/api', apiRoutes)
+app.use('/', htmlRoutes);
+app.use('/api', apiRoutes);
 
-module.exports = app
+module.exports = app;
